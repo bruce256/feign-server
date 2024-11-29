938f40a668491d385ce62fe5197cbb34ddb2a481
“bruce256” <326375225@qq.com>
2024-11-29 21:29:07 +0800

    gateway

---
+++ gateway/src/main/java/org/lvsheng/GatewayApp.java
@@ -0,0 +1,15 @@
+package org.lvsheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class GatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class);
+        System.out.println("Hello World!");
+    }
+}
